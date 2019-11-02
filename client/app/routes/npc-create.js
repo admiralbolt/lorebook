12df5438aa1121df1a3867b72325dc46fe708031
admiralbolt <aviknecht@gmail.com>
2019-11-02 14:58:11 -0500

    A whole bunch of css stuff, the create-npc route form, and sorted all the css properties with csscomb.

--- client/app/routes/npc-create.js
+++ client/app/routes/npc-create.js
@@ -1,4 +1,17 @@
 import Route from '@ember/routing/route';
+import { inject as service } from '@ember/service';
 
 export default Route.extend({
+  api_data: service('api_data'),
+
+  beforeModel(transition) {
+    this.api_data.setActiveModel('npc');
+  },
+
+  actions: {
+    create() {
+
+    }
+  }
+
 });
