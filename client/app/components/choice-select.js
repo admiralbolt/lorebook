bbc75f0478500eeafd0a90aada78c62078d5d2dc
admiralbolt <aviknecht@gmail.com>
2019-11-09 23:24:37 -0600

    Places creation now works as intended.

--- client/app/components/choice-select.js
+++ client/app/components/choice-select.js
@@ -4,6 +4,18 @@ export default Component.extend({
   choices: [],
   value: null,
 
+  init() {
+    this._super(...arguments);
+    if (this.choices.length == 0) return;
+
+    // I don't really understand this, but in order for this to work I need to
+    // run it through a timeout function. Seems like the extra time just from
+    // wrapping in setTimeout is enough to make it work...
+    setTimeout(function() {
+      this.set('value', this.choices[0]);
+    }.bind(this), 0);
+  },
+
   actions: {
     setSelection(value) {
       this.set('value', value);
