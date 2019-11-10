76968e029244d4c3820f4b4d86b27c07e8cf9d0e
admiralbolt <aviknecht@gmail.com>
2019-11-10 11:05:14 -0600

    Lore creation adn editing works. Damn.

--- client/app/components/lore-form.js
+++ client/app/components/lore-form.js
@@ -2,6 +2,7 @@ import Component from '@ember/component';
 
 export default Component.extend({
   lore: null,
+  loreTypes: ['Book', 'General', 'Letter', 'Story'],
 
   didReceiveAttrs() {
     this._super(...arguments);
