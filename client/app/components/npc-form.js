7f6ed2602693d87b5634ea5141912957e2598b6b
admiralbolt <aviknecht@gmail.com>
2019-11-20 16:42:36 -0600

    Aliases now work as intended...

--- client/app/components/npc-form.js
+++ client/app/components/npc-form.js
@@ -1,10 +1,23 @@
 import Component from '@ember/component';
+import { isNone } from '@ember/utils';
 
 export default Component.extend({
   npc: null,
 
+  // Custom tracking for aliases, we need to convert this to an array before posting.
+  aliases_string: '',
+
   didReceiveAttrs() {
     this._super(...arguments);
     this.set('npc', this.get('model') || {});
+    if (!isNone(this.get('model.aliases'))) {
+      this.set('aliases_string', this.get('model.aliases').join('\n'));
+    }
+  },
+
+  actions: {
+    updateAliases: function() {
+      this.set('npc.aliases', this.get('aliases_string').split('\n'));
+    }
   }
 });
