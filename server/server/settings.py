a217be72a2c51bd2b8967a57e3ca2a0a30eecdeb
Avi Knecht <aviknecht@gmail.com>
2021-01-24 15:24:09 -0600

    Dev settings woohoo.

--- server/server/settings.py
+++ server/server/settings.py
@@ -168,3 +168,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
+
+if os.environ.get('DJANGO_DEVELOPMENT'):
+  from .settings_dev import *
---
+++ server/server/settings_dev.py
@@ -0,0 +1,2 @@
+MEDIA_ROOT = '/Users/admiralbolt/git/lorebook/server/uploads'
+STATIC_ROOT = '/Users/admiralbolt/git/lorebook/server/static'
